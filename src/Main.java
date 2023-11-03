25538ca7f91c9dbf35007d280a823d01108b32ad
Aleksandr Maevskiy <aleks.maewsky@yandex.ru>
2023-11-03 23:54:03 +0300

    new commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amountTicket = 29999;
+        int countPay = 20;
+        int bonusMiles = (amountTicket / countPay);
+
+        System.out.println(bonusMiles);
+    }
+}
